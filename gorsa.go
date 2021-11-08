9b0220adbd7f1196d731e7ccf42bcbdf35de4e22
letion <letion@sohu.com>
2021-11-08 15:24:43 +0800

    修改返回值为[]byte

--- gorsa.go
+++ gorsa.go
@@ -5,35 +5,35 @@ import (
 )
 
 // 公钥加密
-func PublicEncrypt(data, publicKey string) (string, error) {
+func PublicEncrypt(data, publicKey string) ([]byte, error) {
 
 	grsa := RSASecurity{}
 	grsa.SetPublicKey(publicKey)
 
 	rsadata, err := grsa.PubKeyENCTYPT([]byte(data))
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 
-	return base64.StdEncoding.EncodeToString(rsadata), nil
+	return rsadata, nil
 }
 
 // 私钥加密
-func PriKeyEncrypt(data, privateKey string) (string, error) {
+func PriKeyEncrypt(data, privateKey string) ([]byte, error) {
 
 	grsa := RSASecurity{}
 	grsa.SetPrivateKey(privateKey)
 
 	rsadata, err := grsa.PriKeyENCTYPT([]byte(data))
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 
-	return base64.StdEncoding.EncodeToString(rsadata), nil
+	return rsadata, nil
 }
 
 // 公钥解密
-func PublicDecrypt(data, publicKey string) (string, error) {
+func PublicDecrypt(data, publicKey string) ([]byte, error) {
 
 	databs, _ := base64.StdEncoding.DecodeString(data)
 
@@ -42,15 +42,15 @@ func PublicDecrypt(data, publicKey string) (string, error) {
 
 	rsadata, err := grsa.PubKeyDECRYPT([]byte(databs))
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 
-	return string(rsadata), nil
+	return rsadata, nil
 
 }
 
 // 私钥解密
-func PriKeyDecrypt(data, privateKey string) (string, error) {
+func PriKeyDecrypt(data, privateKey string) ([]byte, error) {
 
 	databs, _ := base64.StdEncoding.DecodeString(data)
 
@@ -59,8 +59,8 @@ func PriKeyDecrypt(data, privateKey string) (string, error) {
 
 	rsadata, err := grsa.PriKeyDECRYPT([]byte(databs))
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 
-	return string(rsadata), nil
+	return rsadata, nil
 }
